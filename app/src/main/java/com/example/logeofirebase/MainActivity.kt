c2debcc48a16ed982ac0250c1a5ce6f1a6f88b07
Cristofer Antonio Berganza Portillo <antoniocristofer577@gmail.com>
2024-10-20 21:51:22 -0600

    ProyectoFirebase

---
+++ app/src/main/java/com/example/logeofirebase/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.logeofirebase
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.logeofirebase.navigation.NavManager
+import com.example.logeofirebase.ui.theme.LogeoFirebaseTheme
+import com.example.logeofirebase.view.login.TabsView
+import com.example.logeofirebase.viewmodel.LoginViewModel
+import com.example.logeofirebase.viewmodel.NotesViewModel
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        val loginViewModel: LoginViewModel by viewModels()
+        val notesViewModel: NotesViewModel by viewModels()
+        setContent {
+            LogeoFirebaseTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    NavManager(loginViewModel = loginViewModel, notesViewModel = NotesViewModel())
+                }
+            }
+        }
+    }
+}
+
